4441c97e2aa6ad1f67359e1faca4194b142af357
safvanvaliyakath <safvanvaliyakath@gmail.com>
2024-07-30 13:23:45 -0400

    committing all files

---
+++ README.md
@@ -0,0 +1,8 @@
+# schat
+Group based chat app in Flutter and Firebase with Cloudfirestore, Firebase Auth and StreamBuilders 🔥
+ 
+
+
+
+
+
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:188850036653:web:a6339e7ee5f52273e2cb24";
+  static String apiKey = "AIzaSyBExRKx5Iul5gYtkHjJQ8ts4mS9rxfTaUg";
+  static String messagingSenderId = "188850036653";
+  static String projectId = "chatappflutter-40107";
+  final primaryColor = const Color(0xFFee7b64);
+}
